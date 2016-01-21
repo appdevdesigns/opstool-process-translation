1a00dcc9e10971879b13788bb94529eec227d476
Johnny Hausman <johnny@hausmanfamily.com>
2016-01-21 11:42:06 +0700

    + TRRequest.afterUpdate() posts data in a smaller format

--- api/models/TRRequest.js
+++ api/models/TRRequest.js
@@ -52,23 +52,45 @@ module.exports = {
 
       // Compile data to return to the calling application
       var returnData = {};
-      if (typeof(updatedRecord.reference) === 'string') {
-          returnData.reference = JSON.parse(updatedRecord.reference); 
-      } else {
-          returnData.reference = updatedRecord.reference;
-      }
 
+
+      // afterUpdate does not provide the json fields as a object
+      // so we have to convert them with JSON.parse() before using them:
+      returnData.reference = JSON.parse( updatedRecord.reference );
       returnData.language_code = updatedRecord.toLanguageCode;
-      
+
+      // convert objectData:
       var objData = updatedRecord.objectData;
-      if (typeof(updatedRecord.objectData) === 'string') {
-          objData = JSON.parse(updatedRecord.objectData);
+      if (typeof objData == 'string') {
+        objData = JSON.parse(objData);
       }
 
+      // we only want to send back the specific fields being updated:
+      // {
+      //     'fieldName1' : 'translation 1',
+      //     'fieldName2' : 'translation 2'
+      // }
       var fields = {};
+
+      // Current objData.form.fields are in format:
+      //   {
+      //       "activity_name": {
+      //           "th": "test.Q",
+      //           "en": "test.Q",
+      //           "undefined": "[object Object]"
+      //       },
+      //       "activity_description": {
+      //           "th": "asdf",
+      //           "en": "asdf",
+      //           "undefined": "[object Object]"
+      //   }
+    
+      // convert to desired fields format:
       _.forOwn(objData.form.data.fields, function(value, key) {
-          fields[key] = value[returnData.language_code];
+        fields[key] = value[returnData.language_code];
       });
+
+
       returnData.fields = fields;
 
       ADCore.queue.publish(updatedRecord.callback, returnData);
